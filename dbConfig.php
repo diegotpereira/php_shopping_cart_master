ea03d678bf38d7ff15c462f95863606486651a2e
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-05-26 11:47:25 -0300

    criação do arquivo de conexão com banco de dados.

---
+++ dbConfig.php
@@ -0,0 +1,18 @@
+<?php
+// Configuração do banco de dados
+
+$dbHost = "localhost";
+$dbUsuario = "root";
+$dbsenha = "root";
+$dbNome = "php_shopping_cart_master";
+
+// Conexão com banco de dados;
+$db = new mysqli($dbHost, $dbUsuario, $dbSenha, $dbNome);
+
+// checar a conexão
+if ($db->connect_error) {
+    # code...
+    die("Falha na Conexão: " . $db->connect_error);
+}
+
+?>
